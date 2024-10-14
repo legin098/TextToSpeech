76f4053bd13a42474790990feaa51719a46f8443
Jesus Antonio Cruz Suarez <jesus.cruz@alternova.com>
2024-10-14 11:20:39 -0500

    first commit

---
+++ TextToSpeech/TextToSpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToSpeechApp.swift
+//  TextToSpeech
+//
+//  Created by Jesus Cruz Suárez on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TextToSpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
